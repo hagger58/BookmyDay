4d7b9770a1f9a6d3951d0ce902ac40b0924eb22f
hagger58 <hagger58@gmail.com>
2018-12-20 13:27:43 +0100

    germen20152018middag

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -3,122 +3,68 @@
 </nav>
 <div class="container">
     <section style="height:80px;"></section>
-        <div class="row" style="text-align:center;">
-        </div>
         <!----------- Footer ------------>
-            <footer class="footer-bs">
-                <div class="row">
-                        {{--  <div class="col-md-5">  --}}
-                    {{--  <div class="col">  --}}
-                        <h2>Book My Day</h2>
-                        <h6>De Online Boekenshop van Nederland</h6>
-                            <div id="ftrLinks" class="clearfix">
-                                                                {{--  <ul>
-                                                                    <li><a href="{{ route('pages.about') }}"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>About Us</a></li>
-                                                                    <li><a href="#"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Adress</a></li>
-                                                                    <li><a href="{{ route('pages.contact') }}"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Contact</a></li>
-                                                                    <li><a href="#"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Privacy Policy</a></li>
-                                                                    <li><a href="#"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Disclaimer</a></li>
-                                                                    <li><a href="#"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Sitemap</a></li>
-                                                                </ul>  --}}
-                                                            {{--  </div>  --}}
-                                                            {{--  <!-- Footer Links ENDS -->  --}}
-                                                    {{--  </div>  --}}
-                                                        {{--  <div class="col-md-5 footer-nav animated fadeInUp">  --}}
-
-<div class="row align-items-start">
-        <div class="col-md-4">
-            <h2>Klantenserivce</h2>
-                <div class="col">
-                    <ul class="pages">
-                        <li><a href="{{ route('pages.about') }}"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>About Us</a></li>
-                        <li><a href="#"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Adress</a></li>
-                        <li><a href="{{ route('pages.contact') }}"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Contact</a></li>
-                        <li><a href="#"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Privacy Policy</a></li>
-                        <li><a href="#"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Disclaimer</a></li>
-                        <li><a href="#"><i class="fa fa-play magenta">&nbsp;&nbsp;</i>Sitemap</a></li>
-                    </ul>
-                </div>
-        </div>
-        <div class="col-md-4">
-            <h2>Klantenserivce</h2>
-                <div class="col">
-                    <ul class="pages">
-                        <li><a href="#">Algemene Voorwaarden</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Cookie Policy</a></li>
-                        <li><a href="#">Bedrijfspolicy</a></li>
-                        <li><a href="#">Veelgestelde vragen</a></li>
-                    </ul>
-                </div>
-        </div>
-        <div class="col-md-4">
-            <h2>Klantenserivce</h2>
-                <div class="col">
-                    <ul class="pages">
-                        <li><a href="#">Verzenden</a></li>
-                        <li><a href="#">Betalen</a></li>
-                        <li><a href="#">Klantenserivce</a></li>
-                        <li><a href="#">Waardebonnen</a></li>
-                    </ul>
-                </div>
-        </div>
-        <div class="col-md-4 footer-social animated fadeInDown">
-            <h2>Follow</h2>
-                <div class="col">
-                    <ul class="social">
-                        <li> <a href="#"> <span style="font-size: 30px; color: Dodgerblue; list-style: none;">
-                            <i class="fab fa-facebook-square"></i>
-                            </span></a> </li>
-                        <li> <a href="#"> <span style="font-size: 30px; color: Dodgerblue;">
-                            <i class="fab fa-instagram"></i></a> </li>
-                            </span></a>  </a> </li>
-                        <li> <a href="#"> <span style="font-size: 30px; color: Dodgerblue;">
-                            <i class="fab fa-youtube"></i> </a> </li>
-                            </span></a>  </a> </li>
-                        <li> <a href="#"> <span style="font-size: 30px; color: Dodgerblue;">
-                            <i class="fab fa-facebook-square"></i>
-                            </span></a>  </a> </li>
-                        <span class="iconic iconic-check" title="check" aria-hidden="true"></span>
-                    </ul>
-                </div>
-        </div>
-                        {{--  <div class="col-md-4 col-sm-3">
-                                <!-- Quick Contact STARTS -->
-                                <div id="quickCntc" class="clearfix">
-
-                                    <h2>Contact</h2>
-
-                                    <form id="frmContact" name="frmContact" action="#" method="post">
-
-                                        <div class="form-group">
-                                            <input type="text" name="txtName" id="txtName" class="form-control" placeholder="Naam">
-                                        </div>
-
-                                        <div class="form-group">
-                                            <input type="email" name="txtEmailId" id="txtEmailId" class="form-control" placeholder="Email">
-                                        </div>
-
-                                        <div class="form-group">
-                                            <textarea id="txtAreaMessage" name="txtAreaMessage" class="form-control" rows="3" placeholder="Bericht"></textarea>
-                                        </div>
-
-                                        <div class="form-group">
-                                            <input type="submit" id="btnSubmit" name="btnSubmit" class="btn btn-primary" value="Submit">
-                                            <input type="reset" id="btnCancel" name="btnCancel" class="btn btn-danger" value="Cancel">
-                                        </div>
-
-                                    </form>
-
-                                </div>
-                                <!-- Quick Contact ENDS -->
-
-                            </div>  --}}
-                            <!-- .col-md-4 ENDS -->
-    </div>
-            </footer>
-
-            <section style="text-align:center; margin:10px auto;"><p>Designed by BookMyDay <p>© 2018, All rights reserved</p></a></p></section>
+        <footer class="footer-bs center-screen">
+            <div class="row">
+                    <div class="col-md-3">
+                        <h4>Book My Day</h4>
+                        <h6 style="font-size:10px">De Online Boekenshop van Nederland</h6>
+                            <div class="col">
+                                <ul class="list-unstyled">
+                                    <li><a href="{{ route('pages.about') }}">About Us</a></li>
+                                    <li><a href="#">Adress</a></li>
+                                    <li><a href="{{ route('pages.contact') }}">Contact</a></li>
+                                    <li><a href="#">Privacy Policy</a></li>
+                                    <li><a href="#">Disclaimer</a></li>
+                                    <li><a href="#">Sitemap</a></li>
+                                </ul>
+                            </div>
+                    </div>
+                    <div class="col-md-3">
+                        <h4>Klantenserivce</h4>
+                            <div class="col">
+                                <ul class="list-unstyled">
+                                    <li><a href="#">Algemene Voorwaarden</a></li>
+                                    <li><a href="#">Privacy Policy</a></li>
+                                    <li><a href="#">Cookie Policy</a></li>
+                                    <li><a href="#">Bedrijfspolicy</a></li>
+                                    <li><a href="#">Veelgestelde vragen</a></li>
+                                </ul>
+                            </div>
+                    </div>
+                    <div class="col-md-3">
+                        <h4>Aankoop</h4>
+                            <div class="col">
+                                <ul class="list-unstyled">
+                                    <li><a href="#">Verzenden</a></li>
+                                    <li><a href="#">Betalen</a></li>
+                                    <li><a href="#">Klantenserivce</a></li>
+                                    <li><a href="#">Waardebonnen</a></li>
+                                </ul>
+                            </div>
+                    </div>
+                    <div class="col-md-3">
+                        <h4>Follow</h3>
+                            <div class="col">
+                                <ul class="list-unstyled">
+                                    <li> <a href="#"> <span style="font-size: 30px; color: Dodgerblue; list-style: none;">
+                                        <i class="fab fa-facebook-square"></i>
+                                        </span></a> </li>
+                                    <li> <a href="#"> <span style="font-size: 30px; color: Dodgerblue;">
+                                        <i class="fab fa-instagram"></i></a> </li>
+                                        </span></a>  </a> </li>
+                                    <li> <a href="#"> <span style="font-size: 30px; color: Dodgerblue;">
+                                        <i class="fab fa-youtube"></i> </a> </li>
+                                        </span></a>  </a> </li>
+                                    <li> <a href="#"> <span style="font-size: 30px; color: Dodgerblue;">
+                                        <i class="fab fa-facebook-square"></i>
+                                        </span></a>  </a> </li>
+                                    <span class="iconic iconic-check" title="check" aria-hidden="true"></span>
+                                </ul>
+                            </div>
+                    </div>
+                {{--  </div>  --}}
+            </div>
+        </footer>
+    <section style="text-align:center; margin:10px auto;"><p>Designed by BookMyDay <p>© 2018, All rights reserved</p></a></p></section>
 </div>
-
-
